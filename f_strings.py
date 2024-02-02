2b4ee718417ed1d46192ef3eb2135864bda5d707
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-02 15:40:31 +0530

    Create f_strings.py

---
+++ f_strings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+name=input("Enter your name:\n")
+age=input("Enter your age:\n")
+
+
+print("Hello " + name + ". Your age is: " + str(age))
+print(f"hello {name}. your age is {age}")
