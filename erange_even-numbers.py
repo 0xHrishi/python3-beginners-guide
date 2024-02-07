fa46da0de8689dbef2a5566cb4dfc2cf7120274a
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-07 12:45:12 +0530

    Create erange_even-numbers.py

---
+++ erange_even-numbers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+#takes two numbers as user input and use the range method to display only even numbers
+
+first=input("Enter the first number:\n")
+second=input("Enter the second number:\n")
+
+
+if (len(first)<=0) or (len(second)<=0):
+    if (len(first)>0) and (len(second)<=0):
+        print ("Second number field is empty")
+    elif (len(first)<=0) and (len(second)>0):
+        print ("First number field is empty")
+    else:
+        print ("Issues with the user input")
+
+
+elif(len(first)>0) and (len(second)>0):
+    print("*" * 30)
+    if (int(first)< int(second)):
+        for even_number in range(int(first),int(second)):
+            if(int(even_number%2==0)):
+               print("Number is " + str(even_number))
+            else:
+               even_number=int(even_number)+1
+               continue
+
+    else:
+        print("First number is greater than second")
+        print("Cant do anything")
