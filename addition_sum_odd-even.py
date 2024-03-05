3a3ac66100fd26541001d9b52f5a33f3c6c6c84a
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-03-05 05:49:22 +0000

    Update addition_sum_odd-even.py

--- addition_sum_odd-even.py
+++ addition_sum_odd-even.py
@@ -26,3 +26,30 @@ elif(len(first)>0) and (len(second)>0):
     else:
         print(f"{first} plus {second} is --> {ans}")
         print("Sum is odd number")
+------------------------------------------------------------------------------------------------------------------------
+#!/usr/bin/python3
+
+first_number=input("Enter the first number:\n")
+second_number=input("Enter the second number:\n")
+
+if (len(first_number)<=0) or (len(second_number)<=0):
+    if (len(first_number)>0) and (len(second_number)<=0):
+        print("Second number field is empty")
+    elif (len(first_number)<=0) and (len(second_number)>0):
+        print("First number field is empty")
+    else:
+        print("Issues with the user input")
+
+elif (len(first_number)>0) and (len(second_number)>0):
+    print("*"*40)
+    print("First number is " + first_number )
+    print("Second number is " + second_number )
+    print("*"*40)
+    ans=int(first_number)+int(second_number)
+    if(ans%2==0):
+        print("Addition is " + str(ans))
+        print("Result is even number")
+    else:
+        print("Addition is " + str(ans))
+        print("Result is odd number")
+
