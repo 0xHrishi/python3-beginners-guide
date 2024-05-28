8e03fbfcc4e255376b7398aea458a4218aa18b75
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-28 12:46:17 +0530

    Update basic_while_loop.py

--- basic_while_loop.py
+++ basic_while_loop.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
+#basic while loop
+#first number is multipliy by multiplier which is increase by 1 byevey while loop iteration and stop when the result reached is equal to stop number
 
-first=input("Enter the first number:\n")
-multiply=input("Enter the multiply number:\n")
-stop_number=input("Enter the stop number:\n")
+import time
 
 
-if (len(first)<=0) or (len(multiply)<=0) or (len(stop_number)<=0):
-    if (len(first)>0) and (len(multiply)<=0) and (len(stop_number)<=0):
-        print("*" * 20)
-        print ("Multiplier number is empty")
-        print ("Stop number is empty")
-    elif (len(first)>0) and (len(multiply)>0) and (len(stop_number)<=0):
-        print("*" * 20)
-        print ("Stop number is empty")
-    elif (len(first)>0) and (len(multiply)<=0) and (len(stop_number)>0):
-        print("*" * 20)
-        print ("Multiplier number is empty")
-    elif (len(first)<=0) and (len(multiply)>0) and (len(stop_number)<=0):
-        print("*" * 20)
-        print ("First number is empty")
-        print ("Stop number is empty")
-    elif (len(first)>0) and (len(multiply)>0) and (len(stop_number)<=0):
-        print("*" * 20)
-        print ("Stop number is empty")
-    elif (len(first)<=0) and (len(multiply)>0) and (len(stop_number)>0):
-        print("*" * 20)
-        print ("First number is empty")
-    elif (len(first)<=0) and (len(multiply)<=0) and (len(stop_number)>0):
-        print("*" * 20)
-        print ("First number is empty")
-        print ("Multiplier number is empty")
+f_number=input("Enter the first number\n")
+multiplier=input("Enter the multiplier number\n")
+stop_number=input("Enter the stop number\n")
+
+if (len(f_number)<=0) or (len(multiplier)<=0) or (len(stop_number)<=0):
+    if (len(f_number)>0) and (len(multiplier)>0) and (len(stop_number)<=0):
+        print("*"*40)
+        print("Stop number field is empty")
+    elif (len(f_number)>0) and (len(multiplier)<=0) and (len(stop_number)<=0):
+        print("*"*40)
+        print("Multiplier field is empty")
+        print("Stop number field is empty")
+    elif (len(f_number)>0) and (len(multiplier)<=0) and (len(stop_number)>0):
+        print("*"*40)
+        print("Multiplier field is empty")
+    elif (len(f_number)<=0) and (len(multiplier)>0) and (len(stop_number)<=0):
+        print("*"*40)
+        print("First number field is empty")
+        print("Stop number field is empty")
+    elif (len(f_number)<=0) and (len(multiplier)<=0) and (len(stop_number)>0):
+        print("*"*40)
+        print("First number field is empty")
+        print("Multiplier number field is empty")
+    elif (len(f_number)<=0) and (len(multiplier)<=0) and (len(stop_number)<=0):
+        print("*"*40)
+        print("First number field is empty")
+        print("Multiplier field is empty")
+        print("Stop number field is empty")
+elif (len(f_number)>0) and (len(multiplier)>0) and (len(stop_number)>0):
+    print("*"*40)
+    ans=int(f_number)*int(multiplier)
+    if(int(ans) < int(stop_number)):
+        while (int(ans) < int(stop_number)):
+            print (str(f_number) + " times " + str(multiplier) + " is " + str(ans))
+            multiplier=int(multiplier)+1
+            ans=int(f_number)*int(multiplier)
+            time.sleep(0.3)
+
+    elif(int(ans) >= int(stop_number)):
+        print(str(ans) + " is greather than " + str(stop_number))
+        print("Nothing to do")
     else:
-        print ("issues with the user input")
-elif(len(first)>0) and (len(multiply)>0) and (len(stop_number)>0):
-    print ("*"* 20)
-    ans=int(first)*int(multiply)
-    if(int(ans)<int(stop_number)):
-       while(int(ans)<int(stop_number)):
-            ans=int(first)*int(multiply)
-            print(f"{first} times {multiply} is {ans}")
-            multiply=int(multiply)+1
-            ans=int(first)*int(multiply)
-
-    else:
-       print(f"{ans} is greater than {stop_number}")
-       print("Can't go futher")
+        print("Something went wrong")
