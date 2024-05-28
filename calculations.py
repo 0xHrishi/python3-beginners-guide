1426fcecae10020e4b9bee2338c7186e6d642efb
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-28 11:52:45 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -39,3 +39,43 @@ elif (len(f_number)>0) and (len(s_number)>0):
         print("Multiplication result is --> " + str(ans) )
     else:
         print("Invalid choice")
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/usr/bin/python3
+
+f_number=input("Enter the first number --> \n")
+s_number=input("Enter the second number --> \n")
+
+if (len(f_number)<=0) or (len(s_number)<=0):
+    if (len(f_number)>0) and (len(s_number)<=0):
+        print("*"*20)
+        print("Second number field is empty")
+    elif (len(f_number)<=0) and (len(s_number)>0):
+        print("*"*20)
+        print("First number field is empty")
+    elif (len(f_number)<=0) and (len(s_number)<=0):
+        print("*"*20)
+        print("First number field is empty")
+        print("Second number field is empty")
+    else:
+        print("Issues with the user input")
+elif (len(f_number)>0) and (len(s_number)>0):
+    print("*"*40)
+    print("Press 1 --> Addition")
+    print("Press 2--> Subtraction")
+    print("Press 3 --> Multiplication")
+    choice=input("Enter your choice -->")
+    if(str(choice) == "1" ):
+        print("*"*40)
+        ans=float(f_number)+float(s_number)
+        print("Addition result is --> " + str(ans) )
+    elif(str(choice) == "2" ):
+        print("*"*40)
+        ans=float(f_number)-float(s_number)
+        print("Subtraction result is --> " + str(ans) )
+    elif(str(choice) == "3" ):
+        print("*"*40)
+        ans=float(f_number)*float(s_number)
+        print("Multiplication result is --> " + str(ans) )
+    else:
+        print("Invalid choice")
+
