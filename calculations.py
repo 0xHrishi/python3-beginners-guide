0102137ebd68b4ce0df2b4115d0170c7a4279855
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-24 13:04:09 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -43,102 +43,105 @@ elif (len(f_number)>0) and (len(s_number)>0):
 #!/usr/bin/python3
 #with functions
 
+#!/usr/bin/python3
+
 def user_input():
     global first
     global second
     first=input("Enter the first number\n")
     second=input("Enter the second number\n")
 
-def user_validation_1():
-    if (len(first)<=0) or (len(second)<=0):
-        if (len(first)>0) and (len(second)<=0):
-            print("*"*50)
+def validation_1():
+    if (len(first)==0) or (len(second)==0):
+        if (len(first)>0) and (len(second)==0):
+            print("*"*30)
             print("Second number field is empty")
             try_again()
-        elif (len(first)<=0) and (len(second)>0):
-            print("*"*50)
+        elif (len(first)==0) and (len(second)>0):
+            print("*"*30)
             print("First number field is empty")
             try_again()
-        elif (len(first)<=0) and (len(second)<=0):
-            print("*"*50)
+        elif (len(first)==0) and (len(second)==0):
+            print("*"*30)
             print("First number field is empty")
             print("Second number field is empty")
             try_again()
         else:
-            print("*"*50)
+            print("*"*30)
             print("Issues with the user input")
             try_again()
 
-def user_validation_2():
+def validation_2():
     if (len(first)>0) and (len(second)>0):
         if not first.isdigit() or not second.isdigit():
             if first.isdigit() and not second.isdigit():
-                print("*"*50)
-                print("Second number field must contain only numeric digits")
+                print("*"*30)
+                print("Second number field must contain only digits")
                 try_again()
             elif not first.isdigit() and second.isdigit():
-                print("*"*50)
-                print("First number field must contain only numeric digits")
+                print("*"*30)
+                print("First number field must contain only digits")
                 try_again()
             elif not first.isdigit() and not second.isdigit():
-                print("*"*50)
-                print("First number field must contain only numeric digits")
-                print("Second number field must contain only numeric digits")
+                print("*"*30)
+                print("First number field must contain only digits")
+                print("Second number field must contain only digits")
                 try_again()
             else:
-                print("*"*50)
-                print("Issues with the user input validation")
+                print("*"*30)
+                print("Issues with user input validation")
                 try_again()
+
         elif first.isdigit() and second.isdigit():
             print("*"*50)
-            print("Press 1 for Addition")
-            print("Press 2 for Subtraction")
-            print("Press 3 for Multiply")
-            print("Press 4 for Power")
-            calc=input("******Calculation*******\n")
-            if(calc == "1"):
-                print("*"*50)
+            print("Press 1 for Addition\n")
+            print("Press 2 for Subtraction\n")
+            print("Press 3 for Multiply\n")
+            print("Press 4 for Power\n")
+            choice=input("*******Calculation******\n")
+            if (choice=="1"):
+                print("*"*40)
                 ans=int(first)+int(second)
-                print(f"{first} plus {second} is {ans}")
+                print(f"{first} plus {second} --> {ans}")
                 try_again()
-            elif(calc == "2"):
-                print("*"*50)
+            elif (choice=="2"):
+                print("*"*40)
                 ans=int(first)-int(second)
-                print(f"{first} minus {second} is {ans}")
+                print(f"{first} minus {second} --> {ans}")
                 try_again()
-            elif(calc == "3"):
-                print("*"*50)
+            elif (choice=="3"):
+                print("*"*40)
                 ans=int(first)*int(second)
-                print(f"{first} multiply {second} is {ans}")
+                print(f"{first} multiply {second} --> {ans}")
                 try_again()
-            elif(calc == "4"):
-                print("*"*50)
+            elif (choice=="4"):
+                print("*"*40)
                 ans=int(first)**int(second)
-                print(f"{first} power {second} is {ans}")
+                print(f"{first} power {second} --> {ans}")
                 try_again()
             else:
-                print("*"*50)
+                print("*"*40)
                 print("Invalid user input")
                 try_again()
 
+
 def try_again():
-    print("*"*50)
-    choice=input("Would you like to try again y/n?\n")
-    if(choice=="y"):
-        print("*"*50)
+    again=input("Would you like to try again y/n\n")
+    if(again=="y"):
         user_input()
-        user_validation_1()
-        user_validation_2()
-    elif(choice=="n"):
-        print("*"*50)
-        print("You choose to quit")
+        validation_1()
+        validation_2()
+    elif(again=="n"):
+        print("*"*40)
+        print("You choose to quit, bye")
     else:
-        print("*"*50)
-        print("Invalid user choice")
+        print("*"*40)
+        print("Invalid user input for try again")
+
+
 
 
 
 user_input()
-user_validation_1()
-user_validation_2()
-
+validation_1()
+validation_2()
