027b7f6390fb6e3af76570fff528a798e7375087
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-24 12:44:21 +0530

    Update basic_while_loop.py

--- basic_while_loop.py
+++ basic_while_loop.py
@@ -51,83 +51,91 @@ elif (len(f_number)>0) and (len(multiplier)>0) and (len(stop_number)>0):
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 #!/usr/bin/python3
 
-first=input("Enter the first number\n")
-second=input("Enter the second number\n")
-stop_number=input("Enter the stop number\n")
+#!/usr/bin/python3
 
-if (len(first)<=0) or (len(second)<=0) or (len(stop_number)<=0):
-    if (len(first)>0) and (len(second)<=0) and (len(stop_number)<=0):
-        print("*"*20)
-        print("Second number field is empty")
+import time
+
+first=input("Enter the first number:\n")
+multiplier=input("Enter the multiplier number:\n")
+stop_number=input("Enter the stop number:\n")
+
+if (len(first)==0) or (len(multiplier)==0) or (len(stop_number)==0):
+    if (len(first)>0) and (len(multiplier)==0) and (len(stop_number)==0):
+        print("*"*30)
+        print("Multiplier number field is empty")
         print("Stop number field is empty")
-    elif (len(first)>0) and (len(second)>0) and (len(stop_number)<=0):
-        print("*"*20)
+    elif (len(first)>0) and (len(multiplier)>0) and (len(stop_number)==0):
+        print("*"*30)
         print("Stop number field is empty")
-    elif (len(first)>0) and (len(second)<=0) and (len(stop_number)>0):
-        print("*"*20)
-        print("Second number field is empty")
-    elif (len(first)<=0) and (len(second)<=0) and (len(stop_number)>0):
-        print("*"*20)
+    elif (len(first)>0) and (len(multiplier)==0) and (len(stop_number)>0):
+        print("*"*30)
+        print("Multiplier number field is empty")
+    elif (len(first)==0) and (len(multiplier)>0) and (len(stop_number)>0):
+        print("*"*30)
         print("First number field is empty")
-        print("Second number field is empty")
-    elif (len(first)<=0) and (len(second)>0) and (len(stop_number)>0):
-        print("*"*20)
+    elif (len(first)==0) and (len(multiplier)==0) and (len(stop_number)>0):
+        print("*"*30)
         print("First number field is empty")
-    elif (len(first)<=0) and (len(second)>0) and (len(stop_number)<=0):
-        print("*"*20)
+        print("Multiplier number field is empty")
+    elif (len(first)==0) and (len(multiplier)>0) and (len(stop_number)==0):
+        print("*"*30)
         print("First number field is empty")
         print("Stop number field is empty")
-    elif (len(first)<=0) and (len(second)<=0) and (len(stop_number)<=0):
-        print("*"*20)
+    elif (len(first)==0) and (len(multiplier)==0) and (len(stop_number)==0):
+        print("*"*30)
         print("First number field is empty")
-        print("Second number field is empty")
+        print("Multiplier number field is empty")
         print("Stop number field is empty")
-if (len(first)>0) and (len(second)>0) and (len(stop_number)>0):
-    if not first.isdigit() or not second.isdigit() or not stop_number.isdigit():
-        if first.isdigit() and not second.isdigit() and not stop_number.isdigit():
-            print("*"*20)
-            print("Second number must contain only numeric digits")
-            print("Stop number must contain only numeric digits")
-        elif first.isdigit() and second.isdigit() and not stop_number.isdigit():
-            print("*"*20)
-            print("Stop number must contain only numeric digits")
-        elif first.isdigit() and not second.isdigit() and stop_number.isdigit():
-            print("*"*20)
-            print("Second number must contain only numeric digits")
-        elif not first.isdigit() and not second.isdigit() and stop_number.isdigit():
-            print("*"*20)
-            print("First number must contain only numeric digits")
-            print("Second number must contain only numeric digits")
-        elif not first.isdigit() and second.isdigit() and stop_number.isdigit():
-            print("*"*20)
-            print("First number must contain only numeric digits")
-        elif not first.isdigit() and second.isdigit() and not stop_number.isdigit():
-            print("*"*20)
-            print("First number must contain only numeric digits")
-            print("Stop number must contain only numeric digits")
-        elif not first.isdigit() and not second.isdigit() and not stop_number.isdigit():
-            print("*"*20)
-            print("First number must contain only numeric digits")
-            print("Second number must contain only numeric digits")
-            print("Stop number must contain only numeric digits")
-    elif first.isdigit() and second.isdigit() and stop_number.isdigit():
-        ans=int(first)*int(second)
-        if(int(ans)<int(stop_number)):
-           while(int(ans)<int(stop_number)):
-                 print("*"*20)
-                 print(str(first) + " times " + str(second) + " is --> " + str(ans))
-                 second=int(second)+1
-                 ans=int(first)*int(second)
-        elif(int(ans)>int(stop_number)):
-             print("*"*20)
-             print(str(first) + " times " + str(second) + " is --> " + str(ans) )
-             print("Result is greater than the stop number")
-             print("Nothing can be done")
-        elif(int(ans) == int(stop_number)):
-             print("*"*20)
-             print(str(first) + " times " + str(second) + " is --> " + str(ans) )
-             print("Result is equal to stop number")
-             print("Nothing can be done")
+    else:
+        print("*"*30)
+        print("Issues with the user input")
+elif (len(first)>0) and (len(multiplier)>0) and (len(stop_number)>0):
+    if not first.isdigit() or not multiplier.isdigit() or not stop_number.isdigit():
+        if first.isdigit() and not multiplier.isdigit() and not stop_number.isdigit():
+            print("*"*30)
+            print("Mutiplier number field must contain only digits")
+            print("Stop number field must contain only digits")
+        elif first.isdigit() and multiplier.isdigit() and not stop_number.isdigit():
+            print("*"*30)
+            print("Stop number field must contain only digits")
+        elif first.isdigit() and not multiplier.isdigit() and stop_number.isdigit():
+            print("*"*30)
+            print("Multiplier number field must contain only digits")
+        elif not first.isdigit() and multiplier.isdigit() and stop_number.isdigit():
+            print("*"*30)
+            print("First number field must contain only digits")
+        elif not first.isdigit() and not multiplier.isdigit() and stop_number.isdigit():
+            print("*"*30)
+            print("First number field must contain only digits")
+            print("Multiplier number field must contain only digits")
+        elif not first.isdigit() and multiplier.isdigit() and not stop_number.isdigit():
+            print("*"*30)
+            print("First number field must contain only digits")
+            print("Stop number field must contain only digits")
+        elif not first.isdigit() and not multiplier.isdigit() and not stop_number.isdigit():
+            print("*"*30)
+            print("First number field must contain only digits")
+            print("Multiplier number field must contain only digits")
+            print("Stop number field must contain only digits")
         else:
-             print("*"*20)
-             print("Issues with calculation")
+            print("*"*30)
+            print("Issues with user input validation")
+    elif first.isdigit() and multiplier.isdigit() and  stop_number.isdigit():
+        ans=int(first)*int(multiplier)
+        if(int(ans)<int(stop_number)):
+            while(int(ans)<int(stop_number)):
+                print(f"{first} times {multiplier} --> {ans}")
+                multiplier=int(multiplier)+1
+                ans=int(first)*int(multiplier)
+                time.sleep(1)
+        elif(int(ans)==int(stop_number)):
+            print("*"*30)
+            print(f"{first} times {multiplier} --> {ans}")
+            print(f"{ans} is equal to {stop_number}")
+            print("Nothing can be done")
+        elif(int(ans)>int(stop_number)):
+            print("*"*30)
+            print(f"{first} times {multiplier} --> {ans}")
+            print(f"{ans} is greater than {stop_number}")
+            print("Nothing can be done")
+
