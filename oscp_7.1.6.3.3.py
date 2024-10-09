347f6cdebe9b55bceb333341c45cd13871357449
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-09 12:56:05 +0530

    Update oscp_7.1.6.3.3.py

--- oscp_7.1.6.3.3.py
+++ oscp_7.1.6.3.3.py
@@ -13,3 +13,23 @@ else:
     time.sleep(5)
     dnsrecon=subprocess.run(["dnsrecon", "-d", domain_name, "-t", "axfr"])
     print(dnsrecon.stdout)
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+#!/usr/bin/python3
+
+import subprocess
+import time
+
+domain=input("Enter the domain name\n")
+
+if (len(domain)==0):
+    print("*"*50)
+    print("Domain field empty")
+else:
+    print("*******Attempting zone file transfer using dnsrecon*******")
+    time.sleep(2)
+    zone_transfer=subprocess.run(["dnsrecon", "-d", domain, "-t", "axfr"], capture_output=True,text=True)
+    print("*"*30)
+    print(zone_transfer.stdout)
+    print("*"*30)
+    
