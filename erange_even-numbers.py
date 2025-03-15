085aa502ffc80a8b21fb809f59b80ee01d7b7a08
Hrishi Ghosalkar <87890571+RookieRecon@users.noreply.github.com>
2025-03-15 12:40:50 +0530

    Update erange_even-numbers.py

--- erange_even-numbers.py
+++ erange_even-numbers.py
@@ -1,99 +1,83 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-#takes two numbers as user input and use the range method to display only even numbers
-
-first=input("Enter the first number:\n")
-second=input("Enter the second number:\n")
-
-
-if (len(first)<=0) or (len(second)<=0):
-    if (len(first)>0) and (len(second)<=0):
-        print ("Second number field is empty")
-    elif (len(first)<=0) and (len(second)>0):
-        print ("First number field is empty")
-    else:
-        print ("Issues with the user input")
-
-
-elif(len(first)>0) and (len(second)>0):
-    print("*" * 30)
-    if (int(first)< int(second)):
-        for even_number in range(int(first),int(second)):
-            if(int(even_number%2==0)):
-               print("Number is " + str(even_number))
-            else:
-               even_number=int(even_number)+1
-               continue
-
-    else:
-        print("First number is greater than second")
-        print("Cant do anything")
------------------------------------------------------------------------------------------------------------------------------------------------------
-#!/usr/bin/python3
-
-
-#!/usr/bin/python3
+#user input first and second number
+#print even or odd numbers 
 
 import time
 
-first=input("Enter the first number\n")
-second=input("Enter the second number\n")
+#User input
+first_number=input("Enter first number:\n")
+second_number=input("Enter second number:\n")
 
-if len(first)==0 or len(second)==0:
-    if len(first)>0 and len(second)==0:
-        print("*"*50)
+def lines():
+    print("*"*40)
+
+#user input length empty
+if (len(first_number)==0) or (len(second_number)==0):
+    if (len(first_number)>0) and (len(second_number)==0):
+        lines()
         print("Second number field is empty")
-    elif len(first)==0 and len(second)>0:
-        print("*"*50)
+    elif (len(first_number)==0) and (len(second_number)>0):
+        lines()
         print("First number field is empty")
-    elif len(first)==0 and len(second)==0:
-        print("*"*50)
+    elif (len(first_number)==0) and (len(second_number)==0):
+        lines()
         print("First number field is empty")
         print("Second number field is empty")
     else:
-        print("*"*50)
+        lines()
         print("Issues with the user input")
 
-elif len(first)>0 and len(second)>0:
-    if not first.isdigit() or not second.isdigit():
-        if first.isdigit() and not second.isdigit():
-            print("*"*50)
-            print("Second number field must contain only numeric digits")
-        elif not first.isdigit() and second.isdigit():
-            print("*"*50)
-            print("First number field must contain only numeric digits")
-        elif not first.isdigit() and not second.isdigit():
-            print("*"*50)
-            print("First number field must contain only numeric digits")
-            print("Second number field must contain only numeric digits")
+#User input is not empty
+elif len(first_number)>0 and len(second_number)>0:
+    
+    #check user input must contain only numeric values
+    if not first_number.isdigit() or not second_number.isdigit():
+        if first_number.isdigit() and not second_number.isdigit():
+            lines()
+            print("Second number field must contain only numeric values")
+        elif not first_number.isdigit() and second_number.isdigit():
+            lines()
+            print("First number field must contain only numeric values")
+        elif not first_number.isdigit() and not second_number.isdigit():
+            lines()
+            print("First number field must contain only numeric values")
+            print("Second number field must contain only numeric values")
         else:
-            print("*"*50)
-            print("Issues with the user input validation")
-    elif first.isdigit() and second.isdigit():
-        if int(first)<int(second):
-            print("*"*50)
-            choice=input("Would you like to print even or odd numbers\n")
-            if choice=="even":
-                for even_numbers in range(int(first),int(second)+1):
-                    if int(even_numbers)%2==0:
-                        print(f"Even numbers --> {even_numbers}")
-                        time.sleep(0.5)
+            lines()
+            print("Issues with the user input")
+
+    #user input only nuemric values
+    elif first_number.isdigit() and second_number.isdigit():
+        if int(first_number)<int(second_number):
+            opinion=input("Would you like to print even or odd number\n")
+            if str(opinion)=="even":
+                lines()
+                for even_numbers in range(int(first_number),int(second_number)+1):
+                    if (int(even_numbers)%2==0):
+                        lines()
+                        print(f"Even number --> {even_numbers}")
                     else:
-                        time.sleep(0.5)
+                        time.sleep(1)
                         continue
-            elif choice=="odd":
-                for odd_numbers in range(int(first),int(second)+1):
-                    if int(odd_numbers)%2!=0:
-                        print(f"Odd numbers --> {odd_numbers}")
-                        time.sleep(0.5)
+            elif str(opinion)=="odd":
+                lines()
+                for odd_numbers in range(int(first_number),int(second_number)+1):
+                    if (int(odd_numbers)%2!=0):
+                        lines()
+                        print(f"Odd number --> {odd_numbers}")
                     else:
-                        time.sleep(0.5)
+                        time.sleep(1)
                         continue
-        elif int(first)==int(second):
-            print("*"*50)
-            print(f"{first} is equal to {second}")
-            print("Cannot do anything")
-        elif int(first)>int(second):
-            print("*"*50)
-            print(f"{first} is greather than {second}")
-            print("Cannot do anything")
+            else:
+                lines()
+                print("Issues with the user input")
+
+        elif (int(first_number)>int(second_number)):
+            lines()
+            print("First number cannot be greather than second number")
+        elif (int(first_number)==int(second_number)):
+            lines()
+            print("First number equal to second number")
+            print("Nothing can be done")
+
