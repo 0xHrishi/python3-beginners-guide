8188d78488a41fa2fc7d6a9bf9cf344a471dd493
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-18 17:11:57 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,9 @@
+#Print is a function used to display text
+#Single line comment --> Using single hash symbol
+print("Single line comments")
+
+"""
+Multi line comments
+Python doesn’t truly support multi-line comments — triple quotes are just a "hack"
+"""
+print("Multi line comments")
