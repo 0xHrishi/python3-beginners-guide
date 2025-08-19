d23dc56021458ba48ee220b4adb469e7173af283
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-19 10:34:25 +0530

    Create data_type.py

---
+++ data_type.py
@@ -0,0 +1,14 @@
+#This script is display the data type
+
+number=int(input("Enter your favourite number\n"))
+full_name=input("Enter your full name\n")
+decimal_number=float(input("Enter any decimal value\n"))
+
+#Output the user input and display the data type
+print(f"Your favourite number is --> {number} and data type is {type(number)}")
+print(f"Your Full name is --> {number} and data type is {type(full_name)}")
+print(f"Decimal number is --> {number} and data type is {type(decimal_number)}")
+
+
+
+
