0c20392d78b0e5011cc69808b8519a53ae847e3b
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-19 10:44:45 +0530

    Create budget_calculator.py

---
+++ budget_calculator.py
@@ -0,0 +1,22 @@
+#This script for the budget calculation
+
+income=int(input("Enter the total income\n"))
+
+rent_cost=int(input("Enter the rent\n"))
+transport_cost=int(input("Enter the transport cost\n"))
+groceries_cost=int(input("Enter the groceries cost\n"))
+utilies_cost=int(input("Enter the utilies cost\n"))
+extra_cost=int(input("Enter the extra cost\n"))
+
+total_cost=rent_cost+transport_cost+groceries_cost+utilies_cost+extra_cost
+savings=income-total_cost
+
+print(f"Rent cost --> {rent_cost}")
+print(f"Transport cost --> {transport_cost}")
+print(f"Groceries cost --> {groceries_cost}")
+print(f"Utilies cost --> {utilies_cost}")
+print(f"Extra cost --> {extra_cost}")
+
+print("\n")
+print(f"Total cost --> {total_cost}")
+print(f"Saving --> {savings}")
