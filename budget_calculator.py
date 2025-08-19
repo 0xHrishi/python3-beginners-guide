6eb9a4d70b150abc6f934dd3f24b7bcb7de21800
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-19 10:45:57 +0530

    Update budget_calculator.py

--- budget_calculator.py
+++ budget_calculator.py
@@ -8,7 +8,9 @@ groceries_cost=int(input("Enter the groceries cost\n"))
 utilies_cost=int(input("Enter the utilies cost\n"))
 extra_cost=int(input("Enter the extra cost\n"))
 
+#Add all the cost
 total_cost=rent_cost+transport_cost+groceries_cost+utilies_cost+extra_cost
+#Display saving buy subtracting income - total cost
 savings=income-total_cost
 
 print(f"Rent cost --> {rent_cost}")
