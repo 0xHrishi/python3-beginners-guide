20e38d20021a9345e0a5f3d3c2ebacb883f4f205
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 10:43:58 +0530

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,11 +1,11 @@
 #Script to calculate area of the cirlce
 
-#User input --> Enter the radius of the ciccle
+#User input --> Enter the radius of the circle
 radius=float(input("Enter the radius of the circle\n"))
 #Constant 
 PI=3.14
 
-#Calculate the area of circle using formula
+#Calculate the area of circle
 area_of_cicle=PI*radius*radius
 
 #Display the output using formatted string
