31a92a3052499994c783a08a496b6bb611782cf6
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 14:41:26 +0530

    Update email_generator.py

--- email_generator.py
+++ email_generator.py
@@ -1,16 +1,7 @@
-#Python script which takes username as input and display email address
-email_name=input("Enter email name, example --> hrishi.ghosalkar or hrishi_ghosalkar\n")
+email_format=input("Enter the email address format such as hrishi.ghosalkar or hrishi_ghosalkar\n")
 
-#strings --> replace method to replace dot or underscore with space and title to capitalize each word
-full_name=email_name.replace(".", " ").replace("_", " ").title()
+#replace method to replace the dot or underscore with space and title method to capitalize each word 
+full_name=email_format.replace("."," ").replace("_", " ").title()
 
-email_address=f"{full_name}@gmail.com"
-
-print(f"Your full name is {full_name}")
-print(f"Your email address is --> {email_address}")
-
-print("\n")
-print(f"""Dear {full_name}
-Your complete email address is {email_address}.
-Best regards
-""")
+print(f"Your full name is --> {full_name}")
+print(f"Your email address is {email_format}@gmail.com")
