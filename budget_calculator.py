62b92401d3b9f7530be916ef70466a6acb31be56
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 11:50:39 +0530

    Update budget_calculator.py

--- budget_calculator.py
+++ budget_calculator.py
@@ -1,24 +1,19 @@
-#This script for the budget calculation
+#Script for budget calculator
+#Total income --> user input
+total_income=float(input("Enter the total income\n"))
 
-income=int(input("Enter the total income\n"))
+#Costs
+rent_cost=float(input("Enter the rent cost\n"))
+groceries_cost=float(input("Enter the groceries cost\n"))
+transport_cost=float(input("Enter the transport cost\n"))
+utilies_cost=float(input("Enter the utilies cost\n"))
+extra_cost=float(input("Enter the extra cost\n"))
 
-rent_cost=int(input("Enter the rent\n"))
-transport_cost=int(input("Enter the transport cost\n"))
-groceries_cost=int(input("Enter the groceries cost\n"))
-utilies_cost=int(input("Enter the utilies cost\n"))
-extra_cost=int(input("Enter the extra cost\n"))
+#total cost 
+total_expenses=rent_cost+groceries_cost+transport_cost+utilies_cost+extra_cost
+#remaining after income cost - total expenses
+remaining_budget=total_income-total_expenses
 
-#Add all the cost
-total_cost=rent_cost+transport_cost+groceries_cost+utilies_cost+extra_cost
-#Display saving buy subtracting income - total cost
-savings=income-total_cost
-
-print(f"Rent cost --> {rent_cost}")
-print(f"Transport cost --> {transport_cost}")
-print(f"Groceries cost --> {groceries_cost}")
-print(f"Utilies cost --> {utilies_cost}")
-print(f"Extra cost --> {extra_cost}")
-
-print("\n")
-print(f"Total cost --> {total_cost}")
-print(f"Saving --> {savings}")
+print(f"Total income --> {total_income}")
+print(f"Total expenses --> {total_expenses}")
+print(f"Reamining --> {remaining_budget}")
