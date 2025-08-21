dbe3b38177154e2ede88ea07f619b69821793e53
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 11:37:48 +0530

    Update arithmetic_operators.py

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -1,24 +1,19 @@
-#Python script for arithmetic operators
-
 first_number=int(input("Enter the first number\n"))
-second_number=int(input("Enter the first number\n"))
+second_number=int(input("Enter the second number\n"))
 
-#Arithmetic operations such as addition, subtraction, multiplication, power, divide to find quotient and remainder
 add=first_number+second_number
 subtract=first_number-second_number
 multiply=first_number*second_number
 power=first_number**second_number
-quotient_in_decimal=first_number/second_number
-quotient_in_integer=first_number//second_number
+divide_float=first_number/second_number
+divide_integer=first_number//second_number
 remainder=first_number%second_number
 
-print(f"Addition --> {first_number} plus {second_number} --> {add}")
-print(f"Subtraction --> {first_number} subtract {second_number} --> {subtract}")
-print(f"Multiply --> {first_number} multiply {second_number} --> {multiply}")
-print(f"Power -->  {first_number} power {second_number} --> {power}")
-print(f"Quotient in decimal --> {first_number} divide {second_number} --> {quotient_in_decimal}")
-print(f"Quotient in integer --> {first_number} divide {second_number} --> {quotient_in_integer}")
-print(f"Remainder {first_number} divide {second_number} --> {remainder}")
-
-
-
+print(f"First number:{first_number} plus Second number:{second_number} --> {add}")
+print(f"First number:{first_number} subtract Second number:{second_number} --> {subtract}")
+print(f"First number:{first_number} multiply Second number:{second_number} --> {multiply}")
+print(f"First number:{first_number} power Second number:{second_number} --> {power}")
+print(f"First number:{first_number} divide Second number:{second_number} --> Quotient in float value {divide_float}")
+print(f"First number:{first_number} divide Second number:{second_number} --> Quotient in float value {divide_float:.2f}")
+print(f"First number:{first_number} divide Second number:{second_number} --> Quotient in integer value {divide_integer}")
+print(f"First number:{first_number} divide Second number:{second_number} --> Remainder {remainder}")
