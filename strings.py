16c7ecbadeecbc4fb91074c01331a5e59fdffb80
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 11:16:04 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,13 +1,17 @@
+#This script focus on string, concatenation, formatted strings and string popular methods
+
+#User enter his/her full name
 full_name=input("Enter your full name\n")
+#Enter age
+age=input("Enter your age\n")
 
-#Print function to display the text
-print(full_name)
+#Print function to display the user input
+print(full_name, age)
+#String concatenation
+print("Your name is --> " + full_name + " and age is --> " + str(age))
+#Formatted strings
+print(f"Your name is --> {full_name} and age is --> {age}")
 
-#Concatenation
-print("Your full name is " + full_name)
-#formated strings
-print(f"Your full name is {full_name}")
-print("\n")
 
 #Popular string methods
 #upper and lower case, title, capitalize
