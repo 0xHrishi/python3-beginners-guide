3cae6b1e430068b01b868d00eae94b11e732d9b1
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 15:20:39 +0530

    Create assignment_operators.py

---
+++ assignment_operators.py
@@ -0,0 +1,17 @@
+#assignment operators 
+
+#add i.e. first_number=first_number+second_number
+first_number+=second_number						
+#subtract i.e. first_number=first_number-second_number
+first_number-=second_number						
+
+#multiply i.e. first_number=first_number*second_number
+first_number*=second_number					
+
+#quotient i.e. first_number=first_number/second_number
+first_number/=second_number					
+
+#remainder i.e. first_number=first_number%second_number
+first_number%=second_number					
+
+	
