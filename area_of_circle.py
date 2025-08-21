b9c59dda7adc3d967174744cafbdd5823a115280
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 11:04:53 +0530

    Update area_of_circle.py

--- area_of_circle.py
+++ area_of_circle.py
@@ -4,7 +4,6 @@
 radius=float(input("Enter the radius of the circle\n"))
 #Constant
 PI=3.14
-
 #Calculate the area of circle
 area_of_cicle=PI*radius*radius
 
@@ -15,3 +14,7 @@ print("*" * 70)
 #Multiple values
 first_number,second_number,third_number=10,20,30
 print(f"First number --> {first_number}, Second number --> {second_number}, Third number --> {third_number}")
+
+a=b=c=50
+print(f"First number --> {a}, Second number --> {b}, Third number --> {c}")
+
