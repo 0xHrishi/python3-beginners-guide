cfdca54b3d557f4fc16f8121a05712f99deb720a
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-21 16:31:38 +0530

    Update continue_break.py

--- continue_break.py
+++ continue_break.py
@@ -1,15 +1,12 @@
-#As the user to input two numbers and a stop number
-#for loop and if the stop number is found, loop stop and break the iteration else continue until the number is found
-first_number=int(input("Enter the first number\n"))
-second_number=int(input("Enter the second number\n"))
-stop_number=int(input("Enter the stop number\n"))
+first=0
 
-for number in range(first_number, second_number+1):
-    if number == stop_number:
-        print(f"Number is found --> {number}")
+#If the number is equal to 6, the loop stop executing and exit the loop with the help of break statement otherwise continue
+while first <=10:
+    first+=1
+    if first == 6:
+        print("Number 6 is found")
         print("Lets break")
         break
     else:
-        print(f"Number is --> {number}")
-        number+=1
+        print(f"Number is --> {first}")
         continue
