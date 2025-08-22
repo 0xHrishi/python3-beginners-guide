90364992d9da53f0193155c1679167378d172584
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-22 14:17:54 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,56 +1,54 @@
 #functions
 #non parameterized
 
-#def --> Define a function and function name
-print("*" *50 + "Non parameterized functions" + "*" * 50)
-def welcome():
+print("*"*50 + "Non parameterized" + "*"*50)
+def welcome ():
     print("Welcome")
 
-#Call a function
 welcome()
 welcome()
 
-#Parameterized functions
+#paramterized
 #Arguments
-print("*" *50 + "Parameterized functions -- Arguments" + "*" * 50)
+print("*"*50 + "Parameterized --> Arguments" + "*"*50)
 
-def add(first_number,second_number):
+def addition(first_number,second_number):
     sum=first_number+second_number
     print(f"First number:{first_number} plus Second number:{second_number} --> {sum}")
 
-add(50,30)
+addition(15,20)
 
-#Parameterized functions
-#Parameters
-print("*" *50 + "Parameterized functions -- Parameters" + "*" * 50)
-first_number=int(input("Enter first number\n"))
-second_number=int(input("Enter second number\n"))
+# Arguments
+#User input variable and parameters value can be of same name or different
+#for learners same name is ok, but in real projects, different names
+print("*" * 50 + "Parameterized --> Parameters" + "*" * 50)
 
-def add(number_1,number_2):
-    sum=number_1+number_2
-    print(f"First number:{number_1} plus Second number:{second_number} --> {sum}")
+first_number=int(input("Enter the first number\n"))
+second_number=int(input("Enter the second number\n"))
 
-add(first_number,second_number)
+def addition(first_number, second_number):
+    sum = first_number + second_number
+    print(f"First number:{first_number} plus Second number:{second_number} --> {sum}")
 
-#Parameterized functions
-#Return
-print("*" *50 + "Parameterized functions -- Return" + "*" * 50)
-first_number=int(input("Enter first number\n"))
-second_number=int(input("Enter second number\n"))
+addition(first_number,second_number)
 
-def add(number_1,number_2):
-    return number_1+number_2
+# Parameterized - Return
+print("*" * 50 + "Parameterized --> Return value" + "*" * 50)
 
-result=add(first_number,second_number)
-print(f"{result}")
+first_number=int(input("Enter the first number\n"))
+second_number=int(input("Enter the second number\n"))
+
+def addition(first_number, second_number):
+    return first_number + second_number
+
+sum=addition(first_number,second_number)
+print(f"{first_number} plus {second_number} --> {sum}")
 
 #lambda
-#use for small task
-print("*" *50 + "Lambda" + "*" * 50)
-first_number=int(input("Enter first number\n"))
-second_number=int(input("Enter second number\n"))
+print("*" * 50 + "Parameterized -->  Lambda" + "*" * 50)
 
-result=(lambda first_number,second_number:first_number+second_number)(first_number,second_number)
-print(f"{result}")
+first_number=int(input("Enter the first number\n"))
+second_number=int(input("Enter the second number\n"))
 
-
+sum=(lambda first_number,second_number:first_number+second_number)(first_number,second_number)
+print(f"{first_number} plus {second_number} --> {sum}")
