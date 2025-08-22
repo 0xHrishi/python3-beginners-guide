605421ab2402971bc11ead78a297ebc21e163225
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-08-22 13:40:30 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,30 +1,56 @@
 #functions
+#non parameterized
 
-#def means define, welcome() --> function name
-#Non parameterized functions
+#def --> Define a function and function name
+print("*" *50 + "Non parameterized functions" + "*" * 50)
 def welcome():
     print("Welcome")
 
-#call the function
+#Call a function
 welcome()
 welcome()
-
 
 #Parameterized functions
 #Arguments
+print("*" *50 + "Parameterized functions -- Arguments" + "*" * 50)
 
-def add(first,second):
-    sum=first+second
-    print(f"{first} plus {second} --> {sum}")
+def add(first_number,second_number):
+    sum=first_number+second_number
+    print(f"First number:{first_number} plus Second number:{second_number} --> {sum}")
 
-add(15,30)
+add(50,30)
 
-#Paramters
-def addition(first_number,second_number):
-    result=first_number+second_number
-    print(f"{first_number} plus {second_number} --> {result}")
+#Parameterized functions
+#Parameters
+print("*" *50 + "Parameterized functions -- Parameters" + "*" * 50)
+first_number=int(input("Enter first number\n"))
+second_number=int(input("Enter second number\n"))
 
-first=int(input("Enter first number:\n"))
-second=int(input("Enter the second number"))
+def add(number_1,number_2):
+    sum=number_1+number_2
+    print(f"First number:{number_1} plus Second number:{second_number} --> {sum}")
 
-addition(first,second)
+add(first_number,second_number)
+
+#Parameterized functions
+#Return
+print("*" *50 + "Parameterized functions -- Return" + "*" * 50)
+first_number=int(input("Enter first number\n"))
+second_number=int(input("Enter second number\n"))
+
+def add(number_1,number_2):
+    return number_1+number_2
+
+result=add(first_number,second_number)
+print(f"{result}")
+
+#lambda
+#use for small task
+print("*" *50 + "Lambda" + "*" * 50)
+first_number=int(input("Enter first number\n"))
+second_number=int(input("Enter second number\n"))
+
+result=(lambda first_number,second_number:first_number+second_number)(first_number,second_number)
+print(f"{result}")
+
+
