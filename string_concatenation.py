abd2520643c44de94ff4bac54fbb96dfd404b55f
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-10-22 16:04:03 +0530

    Delete string_concatenation.py

--- string_concatenation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-#Simple python3 script for string concatenation. 
-
-print("String Manupulation Exercise")
-print('String concattenation is done with "+" sign')
-print('eg. print("Hello " + " " + "Jenny")')
-print("New lines can be created with a backslash and n")
-
