a1962515a5b96481707bf70665221a96cbd54c10
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-10-22 16:02:17 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,23 +1,10 @@
-#This script focus on string, concatenation, formatted strings
+#String examples 
+full_name = "Hrishi"
 
-#User enter his/her full name
-full_name=input("Enter your full name\n")
-#Enter age
-age=input("Enter your age\n")
-
-#string triple quotes to dislay larger text 
-print("""Hi there,
-
-Note: It's important to ensure that you have created a snapshot of the virtual machine before making any configuration changes. This will allow you to revert to a previous state if needed. Regular snapshots can save a great deal of time and frustration if something goes wrong.
-
-If you have any further questions or need assistance, feel free to ask.
-Please do not forget to rate the course and leave a review
-Thanks and let's keep learning together """)
-#Print function to display the user input
-print(full_name, age)
+lucky_number = 8
 
 #String concatenation
-print("String Concatentation --> Your name is --> " + full_name + " and age is --> " + str(age))
+print("Your full name is " + full_name + " and lucky number is " + str(lucky_number))
 
-#Formatted strings
-print(f"Formatted strings --> Your name is --> {full_name} and age is --> {age}")
+#Formatted output
+print(f"Your full name is {full_name} and lucky number is {lucky_number}")
