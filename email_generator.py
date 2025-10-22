69498ebac50679cc6f33174b6a798d18c59d6f5c
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-10-22 16:35:48 +0530

    Delete email_generator.py

--- email_generator.py
+++
@@ -1,7 +0,0 @@
-email_format=input("Enter the email address format such as hrishi.ghosalkar or hrishi_ghosalkar\n")
-
-#replace method to replace the dot or underscore with space and title method to capitalize each word 
-full_name=email_format.replace("."," ").replace("_", " ").title()
-
-print(f"Your full name is --> {full_name}")
-print(f"Your email address is {email_format}@gmail.com")
