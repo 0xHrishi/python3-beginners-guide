b33639bdba824a5a110ca47423712c37d2cb7808
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-03 12:38:21 +0530

    Create 03_variables.py

---
+++ python/03_variables.py
@@ -0,0 +1,26 @@
+
+# --------------------------------------------------
+# Single variable assignment
+# --------------------------------------------------
+movie_name = "Harry potter"
+lucky_number = 10
+decimal_number = 15.5
+
+print(f"Movie name -- {movie_name}")
+print(f"Lucky number -- {lucky_number}")
+print(f"Decimal number -- {decimal_number}")
+
+
+# --------------------------------------------------
+# Assigning multiple variables in a single statement
+# --------------------------------------------------
+number_1, number_2, number_3 = 10, 20, 30
+print(f"Number 1 -- {number_1}")
+print(f"Number 2 -- {number_2}")
+print(f"Number 3 -- {number_3}")
+
+# --------------------------------------------------
+# Assigning the same value to multiple variables at once
+# --------------------------------------------------
+number_4 = number_5 = 40
+print(f"Number 4 and Number 5 -- {number_4} and {number_5}")
