b8fadcc9f80d6e825015f3ac7717dd8092440c9a
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-09 12:31:19 +0530

    Update and rename 13_if_if_else.py to 16_if_elif_else_statements.py

---
+++ python/16_if_elif_else_statements.py
@@ -0,0 +1,32 @@
+# Basic statements 
+
+# Basic if statement
+if condition:
+    code
+
+# Basic if else statement 
+if condition:
+    code
+else:
+    code
+
+# Basic if else statement 
+if condition:
+    code
+elif condition:
+    code
+elif condition:
+    code
+
+# Nested statements
+if condition:
+    if condition:
+        code
+    elif condition:
+        code
+    else
+        code
+elif condition:
+    code
+elif condition:
+    code
