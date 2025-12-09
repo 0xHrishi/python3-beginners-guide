9d59748f651fcbadb55f00fbb0b01bbb35c533a4
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-09 12:19:41 +0530

    Update 13_logical_operators.py

--- python/13_logical_operators.py
+++ python/13_logical_operators.py
@@ -2,5 +2,5 @@
 # Allows the user to combine and check multiple conditions
 
 # and : Both conditions needs to be true. 
-# or : Out of two conditions, 1 must be true. 
+# or : Out of two or more conditions, 1 must be true. 
 # not : If the condition is true, than return false i.e. reverse result.  
