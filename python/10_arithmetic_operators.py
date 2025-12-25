b05ed4ce613f8cfc4da6e68ba3fb2a2f7ba80932
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2025-12-25 15:03:31 +0530

    Update 10_arithmetic_operators.py

--- python/10_arithmetic_operators.py
+++ python/10_arithmetic_operators.py
@@ -1,62 +1,49 @@
-# Basic arithmetic operations
-# Add, subtract, Multiplication, Division (Quotient and Remainder), power
+#!/usr/bin/python
 
+# Basic Arithmetic operators
+# Function to print line i.e. separator line
 def lines():
-    print("*"*50)
+        print("*"*100)
 
-# Prompt user for input
-first_number = input("Enter the first number\n")
-second_number = input("Enter the second number\n")
+# User input
+first_number = input("Enter the first number:\n")
+second_number = input("Enter the second number:\n")
 
-# If user input is empty
+# If user input missing 
 if len(first_number) == 0 or len(second_number) == 0:
-    if len(first_number) > 0 and len(second_number) == 0:
         lines()
-        print("User input --> Second number field is empty")
-    elif len(first_number) == 0 and len(second_number) > 0:
-        lines()
-        print("User input --> First number field is empty")
-    elif len(first_number) == 0 and len(second_number) == 0:
-        lines()
-        print("User input --> First number field is empty")
-        print("User input --> Second number field is empty")
+        if len(first_number) == 0:
+                print("User input --> First number field is empty")
+        if len(second_number) == 0:
+                print("User input --> Second number field is empty")
 
-# User input is not empty
-# with the help of .lstrip() and .isdigit() string method -- Make sure the user input must be only numeric value
-# Special characters and alphabets not allowed
-# If user input passes the validation -- Perform arithmetic operations 
-
-elif len(first_number) > 0 and len(second_number)>0:
-    if not first_number.lstrip("-").isdigit() or not second_number.lstrip("-").isdigit():
-        if first_number.lstrip("-").isdigit() and not second_number.lstrip("-").isdigit():
-            lines()
-            print("Second number field must contain only numeric values(positive or negative)")
-        elif not first_number.lstrip("-").isdigit() and second_number.lstrip("-").isdigit():
-            lines()
-            print("First number field must contain only numeric values(positive or negative)")
+# User input 
+# Validation check i.e. user input must contain only positive and negative numbers
+# Validation pass -- Perofrm arithmetic operators 
+elif len(first_number) > 0 and len(second_number) > 0:
+        if not first_number.lstrip("-").isdigit() or not second_number.lstrip("-").isdigit():
+                lines()
+                if not first_number.lstrip("-").isdigit():
+                        print("First number field must contain only numeric values")
+                if not second_number.lstrip("-").isdigit():
+                        print("Second number field must contain only numeric values")
         elif first_number.lstrip("-").isdigit() and second_number.lstrip("-").isdigit():
-            lines()
-            print("First number field must contain only numeric values(positive or negative)")
-            print("Second number field must contain only numeric values(positive or negative)")
-
-    if first_number.lstrip("-").isdigit() and second_number.lstrip("-").isdigit():
-        first_number = int(first_number)
-        second_number = int(second_number)
-
-        add = first_number + second_number
-        sub = first_number - second_number
-        multiply = first_number * second_number
-        power = first_number ** second_number
-        quotient = first_number / second_number
-        remainder = first_number % second_number
-
-        print(f"First number: {first_number} Addition Second number: {second_number} --> {add}")
-        print(f"First number: {first_number} Subtract Second number: {second_number} --> {sub}")
-        print(f"First number: {first_number} Multiply Second number: {second_number} --> {multiply}")
-        print(f"First number: {first_number} Power Second number: {second_number} --> {power}")
-        print(f"First number: {first_number} Divide (Quotient) Second number: {second_number} --> {quotient}")
-        print(f"First number: {first_number} Divide (Quotient) Second number: {second_number} --> {quotient:.2f}")
-        print(f"First number: {first_number} Divide (Remainder) Second number: {second_number} --> {remainder}")
+                lines()
+                first_number = int(first_number)
+                second_number = int(second_number)
+                sum = first_number + second_number
+                subtract = first_number - second_number
+                multiply = first_number * second_number
+                power = first_number ** second_number
+                quotient = first_number / second_number
+                quotient_rounding= first_number // second_number
+                remainder = first_number % second_number
 
 
-
+                print(f"First number: {first_number} plus Second number: {second_number} --> {sum}")
+                print(f"First number: {first_number} minus Second number: {second_number} --> {subtract}")
+                print(f"First number: {first_number} multiply Second number: {second_number} --> {multiply}")
+                print(f"First number: {first_number} power Second number: {second_number} --> {power}")
+                print(f"First number: {first_number} divide (Quotient) Second number: {second_number} --> {quotient}")
+                print(f"First number: {first_number} divide (Quotient -- Rounding) Second number: {second_number} --> {quotient:.2f}")
+                print(f"First number: {first_number} divide (Remainder) Second number: {second_number} --> {remainder}")
