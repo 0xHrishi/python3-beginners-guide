2603c2eaf32fbcc255691e1eba10993b2aaaec44
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-01-23 14:04:38 +0530

    Update 05_global_local_variable.py

--- python/05_global_local_variable.py
+++ python/05_global_local_variable.py
@@ -1,41 +1,45 @@
 # ------------------------------------------------------------
 # Program: Demonstrate Local vs Global Variables in Python
 # ------------------------------------------------------------
+#!/usr/bin/python
+# Local variable 
+def lines():
+    print("*"*100)
 
-# Global variable example
+# variable
 number = 10
 
-# Created a function i.e. global_example
-# And within the function used the global keyword to change the value of the number 
-def global_example():
+# function 
+def local_variable_ex():
+    number = 20
+    print(f"Within the function, value of number: {number}")
+
+print(f"Before the function is called, value of number: {number}")
+local_variable_ex()
+print(f"After the function is called, value of number: {number}")
+
+###########################OUTPUT#################################
+# Before the function is called, value of number: 10
+# Within the function, value of number: 20
+# After the function is called, value of number: 10
+##################################################################
+# global variable example
+lines()
+
+number = 50
+
+def global_variable_ex():
     global number
-    number = 20
-    print(f"Within the function, the value of number is -- {number}")
+    number = 100
+    print(f"Within the function, value of number: {number}")
 
-print(f"Before the function is called, the value of number is -- {number}")
-global_example()
-print(f"Function finishes it execution, the value of number is -- {number}")
 
-print("*"*100)
+print(f"Before the function is called, value of number: {number}")
+global_variable_ex()
+print(f"After the function is called, value of number: {number}")
 
-# Local variable example
-number = 10
-
-# Created a function i.e. local_example
-# And within the function created a local variable (only exists inside the function)
-def local_example():
-    number = 20
-    print(f"Within the function, the value of number is -- {number}")
-
-print(f"Before the function is called, the value of number is -- {number}")
-local_example()
-print(f"Function finishes it execution -- value of number is -- {number}")
-
-# ----------------------------OUTPUT----------------------------------
-# Before the function is called, the value of number is -- 10
-# Within the function, the value of number is -- 20
-# Function finishes it execution, the value of number is -- 20
-# ****************************************************************************************************
-# Before the function is called, the value of number is -- 10
-# Within the function, the value of number is -- 20
-# Function finishes it execution -- value of number is -- 10
+###########################OUTPUT#################################
+# Before the function is called, value of number: 50
+# Within the function, value of number: 100
+# After the function is called, value of number: 100
+##################################################################
