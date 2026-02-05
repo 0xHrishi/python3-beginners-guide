5332a3efc2bb64ac6fe193de49132814d9646384
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-05 13:09:48 +0530

    Update 05_global_local_variable.py

--- python/05_global_local_variable.py
+++ python/05_global_local_variable.py
@@ -2,44 +2,38 @@
 # Program: Demonstrate Local vs Global Variables in Python
 # ------------------------------------------------------------
 #!/usr/bin/python
-# Local variable 
-def lines():
-    print("*"*100)
 
-# variable
-number = 10
+number_1 = 10
 
-# function 
-def local_variable_ex():
-    number = 20
-    print(f"Within the function, value of number: {number}")
-
-print(f"Before the function is called, value of number: {number}")
-local_variable_ex()
-print(f"After the function is called, value of number: {number}")
-
-###########################OUTPUT#################################
-# Before the function is called, value of number: 10
-# Within the function, value of number: 20
-# After the function is called, value of number: 10
-##################################################################
-# global variable example
-lines()
-
-number = 50
-
-def global_variable_ex():
-    global number
-    number = 100
-    print(f"Within the function, value of number: {number}")
+def global_variable():
+    global number_1
+    number_1 = 20
+    print(f"Value of number within the function -- > {number_1}")
 
 
-print(f"Before the function is called, value of number: {number}")
-global_variable_ex()
-print(f"After the function is called, value of number: {number}")
 
-###########################OUTPUT#################################
-# Before the function is called, value of number: 50
-# Within the function, value of number: 100
-# After the function is called, value of number: 100
-##################################################################
+print("**********Global variable example**********")
+print(f"Value of number before the function called -- > {number_1}")
+global_variable()
+print(f"Value of number After the function called -- > {number_1}")
+
+#**********Global variable example**********
+# Value of number before the function called -- > 10
+# Value of number within the function -- > 20
+# Value of number After the function called -- > 20
+#**********Global variable example**********
+number_2 = 100
+
+def local_variable():
+    number_2 = 200
+    print(f"Value of number within the function -- > {number_2}")
+
+print("**********Global variable example**********")
+print(f"Value of number before the function called -- > {number_2}")
+local_variable()
+print(f"Value of number after the function called -- > {number_2}")
+#**********Local variable example**********
+# Value of number before the function called -- > 100
+# Value of number within the function -- > 200
+# Value of number after the function called -- > 100
+#**********Local variable example**********
