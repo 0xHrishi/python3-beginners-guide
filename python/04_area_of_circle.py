46b630a2229a9a96ea23beae1101060bf166a8ed
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-06 17:54:28 +0530

    Update 04_area_of_circle.py

--- python/04_area_of_circle.py
+++ python/04_area_of_circle.py
@@ -22,3 +22,33 @@ print(f"Area of the circle --> {area_of_circle}")
 print(f"Area of the circle --> {area_of_circle:.2f}")
 
 --------------------------------------------------------------------------------------------------------
+#!/usr/bin/python
+# calculate area of circle 
+def lines():
+    print("*"*75)
+
+# prompt user input
+radius = input("Enter the radius of the circle\n")
+
+# user input empty
+if len(radius) == 0:
+    lines()
+    print(f"User input --> Radius of the circle field is empty")
+
+# user input not empty
+# validation check i.e. user input must be positive numeric value
+else:
+    lines()
+    if not radius.replace(".", "").isdigit():
+        print("Radius of circle field must contain only positive numeric values")
+    if radius.replace(".", "").isdigit():
+        radius = float(radius)
+        if radius == 0:
+            print(f"Radius of the circle cannot be zero")
+        elif radius < 0:
+            print(f"Radius of the circle cannot be less than zero")
+        else:
+            PI = 3.14
+            area_of_circle = PI * radius ** 2
+            print(f"Radius of circle {radius}")
+            print(f"Area of circle {area_of_circle:.2f}")
