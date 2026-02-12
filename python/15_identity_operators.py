69820bfc0076fa5102fd5bfd6aee75dc828ac1b9
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-02-12 15:11:06 +0530

    Update 15_identity_operators.py

--- python/15_identity_operators.py
+++ python/15_identity_operators.py
@@ -1,4 +1,4 @@
 # Identity operators
 
-# is      --> Checks whether two variables point to the same object in memory
-# is not  --> Checks whether two variables do NOT point to the same object in memory
+is      --> Checks whether two variables point to the same object in memory
+is not  --> Checks whether two variables do NOT point to the same object in memory
