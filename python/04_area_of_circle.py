3b5ea9c362484e5138f4a10bf943692c7e4e8738
Hrishi Ghosalkar <87890571+0xHrishi@users.noreply.github.com>
2026-05-13 23:17:48 +0530

    Update 04_area_of_circle.py

--- python/04_area_of_circle.py
+++ python/04_area_of_circle.py
@@ -20,32 +20,34 @@ print(f"Area of circle --> {area_of_circle:.2f}")
 
 --------------------------------------------------------------------------------------------------------
 #!/usr/bin/python
-# calculate area of circle 
+
+# function to display lines
 def lines():
-    print("*"*75)
+        print("*"*75)
 
-# prompt user input
-radius = input("Enter the radius of the circle\n")
+# user input
+radius = input("Enter the radius of the circle:\n")
 
-# user input empty
+# constant
+PI = 3.14
+
+# check user input
 if len(radius) == 0:
-    lines()
-    print(f"User input --> Radius of the circle field is empty")
-
+        lines()
+        print("User input --> Radius field empty")
 # user input not empty
-# validation check i.e. user input must be positive numeric value
+# validate user input i.e. must contain only numeric values greater than zero
+# validation passed --> calculate area of circle 
 else:
-    lines()
-    if not radius.replace(".", "").isdigit():
-        print("Radius of circle field must contain only positive numeric values")
-    if radius.replace(".", "").isdigit():
-        radius = float(radius)
-        if radius == 0:
-            print(f"Radius of the circle cannot be zero")
-        elif radius < 0:
-            print(f"Radius of the circle cannot be less than zero")
+        lines()
+        if not radius.replace(".","").isdigit():
+                print("Radius field must contain positive numeric values")
         else:
-            PI = 3.14
-            area_of_circle = PI * radius ** 2
-            print(f"Radius of circle {radius}")
-            print(f"Area of circle {area_of_circle:.2f}")
+                radius = float(radius)
+                if radius >= 0:
+                        area_of_circle = PI * radius * radius
+                        print(f"Radius of the circle --> {radius}")
+                        print(f"Area of the circle --> {area_of_circle}")
+                        print(f"Area of the circle --> {area_of_circle:.2f}")
+                else:
+                        print(f"Radius of the circle cannot be less than zero")
